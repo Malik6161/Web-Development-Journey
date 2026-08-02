4cda87ae9b31dd4957c60b690d58b0e7190383e3
Awais <awaismalik781199@gmail.com>
2026-08-02 18:19:55 +0500

    Feet: Learn the strings in js

---
+++ strings.js/index.js
@@ -0,0 +1,21 @@
+console.log("This is the strings practice file.");
+let a = "Malik";
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+//  If we try to access a character that is out of bounds, it will return undefined.
+// console.log(a[5]);
+
+console.log(a.length);
+
+// template literals
+
+let name = "Malik";
+let name2 = "awais";
+console.log(`My name is ${name} and my friend name is ${name2}.`);
+let name3 = "Shazam";
+console.log(name3.toUpperCase());
+console.log(name3.toLowerCase());
