6b714cba6db9bc87d050ad9967274d822e549acd
Awais <awaismalik781199@gmail.com>
2026-08-02 12:46:34 +0500

    Add calculator functionality with JavaScript

---
+++ project1/script.js
@@ -0,0 +1,43 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+let input = "";
+
+buttons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+
+        let value = button.innerText;
+
+        if (value === "AC") {
+            input = "";
+            display.value = "";
+        }
+
+        else if (value === "DEL") {
+            input = input.slice(0, -1);
+            display.value = input;
+        }
+
+        else if (value === "=") {
+
+            try {
+                input = eval(input).toString();
+                display.value = input;
+            }
+
+            catch {
+                display.value = "Error";
+                input = "";
+            }
+
+        }
+
+        else {
+            input += value;
+            display.value = input;
+        }
+
+    });
+
+});
